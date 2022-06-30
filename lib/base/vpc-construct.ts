17bbcfe8c2b830d58ad8ed7d34809ab98d53e5be
EC2 Default User <ec2-user@ip-172-31-16-127.ap-northeast-2.compute.internal>
2022-06-30 02:17:28 +0000

    add security group

--- lib/base/vpc-construct.ts
+++ lib/base/vpc-construct.ts
@@ -39,5 +39,65 @@ export class VpcConstruct extends Construct {
         subnetType: SubnetType.PRIVATE_ISOLATED,
       }]
     })
+    
+    // Create a SG for a web server
+    // const webserverSG = new ec2.SecurityGroup(this, 'web-server-sg', {
+    //   vpc: this.vpc,
+    //   allowAllOutbound: true,
+    //   description: 'security group for a web server',
+    // });
+
+    // webserverSG.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(22),
+    //   'allow SSH access from anywhere',
+    // );
+
+    // webserverSG.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(80),
+    //   'allow HTTP traffic from anywhere',
+    // );
+
+    // webserverSG.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(443),
+    //   'allow HTTPS traffic from anywhere',
+    // );
+
+    // webserverSG.addIngressRule(
+    //   ec2.Peer.ipv4('123.123.123.123/16'),
+    //   ec2.Port.allIcmp(),
+    //   'allow ICMP traffic from a specific IP range',
+    // );
+    
+    // // Create a SG for a backend server
+    // const backendServerSG = new ec2.SecurityGroup(this, 'backend-server-sg', {
+    //   vpc: this.vpc,
+    //   allowAllOutbound: true,
+    //   description: 'security group for a backend server',
+    // });
+    // backendServerSG.connections.allowFrom(
+    //   new ec2.Connections({
+    //     securityGroups: [webserverSG],
+    //   }),
+    //   ec2.Port.tcp(8000),
+    //   'allow traffic on port 8000 from the webserver security group',
+    // );
+
+    // // Create a SG for a database server
+    // const dbserverSG = new ec2.SecurityGroup(this, 'database-server-sg', {
+    //   vpc: this.vpc,
+    //   allowAllOutbound: true,
+    //   description: 'security group for a database server',
+    // });
+
+    // dbserverSG.connections.allowFrom(
+    //   new ec2.Connections({
+    //     securityGroups: [backendServerSG],
+    //   }),
+    //   ec2.Port.tcp(3306),
+    //   'allow traffic on port 3306 from the backend server security group',
+    // );
   }
 }
