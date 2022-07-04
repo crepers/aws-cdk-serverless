6fe3ceabe396d24ff626ee9bc56879213d75ae7c
EC2 Default User <ec2-user@ip-172-31-39-75.ap-northeast-2.compute.internal>
2022-07-04 03:26:06 +0000

    recover config.ts file

---
+++ config/config.ts
@@ -0,0 +1,10 @@
+export namespace App {
+  export const Context = {
+    ns: 'dev',
+    region: 'ap-northeast-2',
+  }
+}
+
+export namespace IdentityProvider {
+  export const RedirectUri = 'http://localhost:3000'
+}
